4a911bfaf9d3306167d03ff1fac73732261ded68
Michel Sommer <skruen@gmail.com>
2023-02-10 11:20:12 +0100

    lesson 1

---
+++ Sesson_1/TestBuild/TestBuild/main.c
@@ -0,0 +1,29 @@
+/*
+ * TestBuild.c
+ *
+ * Created: 10/02/2023 10.13.33
+ * Author : skruen
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB |= (1 << PB7);		// Sets pin as output
+	//PORTB &= ~(1 << PB7);	// Sets pin low
+    
+    while (1) 
+    {
+		
+		PORTB |= (1 << PB7);	// Sets pin high
+		_delay_ms(500);
+		
+		// PORTB &= ~(1 << PB7);	// Sets pin low
+		PORTB &= (0 << PB7);	// Sets pin low
+		_delay_ms(500);
+		
+    }
+}
+
