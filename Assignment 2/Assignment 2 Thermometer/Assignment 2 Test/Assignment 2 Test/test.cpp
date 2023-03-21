fc5879a4f1b757ae88bdbdce3614a9915c7aebc7
Michel Sommer <skruen@gmail.com>
2023-03-21 15:44:42 +0100

    add test project

---
+++ Assignment 2/Assignment 2 Thermometer/Assignment 2 Test/Assignment 2 Test/test.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+/*
+ * IMPORTANT:	Import avr toolchain from: "C:\Program Files (x86)\Arduino\hardware\tools\avr\avr"
+ *				Insted of using: "C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include"
+ */
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
